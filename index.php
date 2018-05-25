e114d2a3502a9a510930db9909217995270f8763
BUP <isratrumman25@gmail.com>
2018-05-25 13:59:54 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>print</title>
+</head>
+<body>
+<h1>My first PHP page</h1>
+
+<?php
+// $text='Hellow World!';
+// $varsityName='BUP';
+// echo $text;
+?>
+<!-- <h2>
+	<?php 
+  // echo $varsityName;
+  ?> Student Rolls:
+</h2> -->
+<?php
+	// for(initialization, condition; increment/decrement) {}
+  // for ($i=1; $i <=10; $i++) { 
+  // 	// if (condition) {
+  // 	// 	# code...
+  // 	// }
+  // 	echo 'Roll: ' . $i;
+  // 	echo '</br>';
+  // }
+?>
+
+<hr>
+<form action="submit.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
